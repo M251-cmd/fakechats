7e2ec5a8e5864fd2b695be6d4ca257006a45cd93
Twometer <twometer@outlook.de>
2018-06-23 17:15:42 +0200

    Resource cleanup, add string resources, add ActionBar

--- app/src/main/java/de/twometer/fakechats/gui/MainActivity.java
+++ app/src/main/java/de/twometer/fakechats/gui/MainActivity.java
@@ -1,8 +1,18 @@
 package de.twometer.fakechats.gui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -29,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.background));
-
         handler = new Handler(getMainLooper());
 
         ListView chatListView = findViewById(R.id.chat_list_view);
@@ -46,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
                 chatInputView.setText("");
             }
         });
+
+        initActionBar();
     }
 
     private void sendMessage(final String content, final MessageSender sender) {
@@ -76,5 +86,50 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
     }
 
+    private void initActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(false);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setDisplayShowCustomEnabled(true);
+            View customView = View.inflate(actionBar.getThemedContext(), R.layout.chat_action_bar, null);
+            ImageView v = customView.findViewById(R.id.contactImage);
+            v.setImageBitmap(getCroppedBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.avatar_contact)));
+            actionBar.setCustomView(customView);
+            Toolbar parent =(Toolbar) customView.getParent();
+            parent.setPadding(0,0,0,0);//for tab otherwise give space in tab
+            parent.setContentInsetsAbsolute(0,0);
+
+        }
+    }
+
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chat_menu, menu);
+        return true;
+    }
 
 }
